38c420896c0a4ab4ac05a0293f3bccf4d8e6f8ee
MrCronkite <vladshima99@gmail.com>
2023-02-05 19:59:42 +0300

    first commit

---
+++ TestTask/TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by admin1 on 5.02.23.
+//
+
+import UIKit
+
+class ViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
